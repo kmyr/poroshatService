b04b494807b34285ed420c081377fc6f57a327ab
kmyr <kmyr.rp@gmail.com>
2020-07-27 16:28:01 +0430

    date generator engine

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <form>
-      <div class="col-md-2">
+      <div class="col-md-12">
         <div class="form-group" id="infoForm">
           <label>نام و نام خانوادگی</label>
-          <input v-model="info.name" type="text" class="form-control" />
+          <input v-model="info.name" type="text" class="form-control" required />
 
           <label>نام پدر</label>
-          <input v-model="info.fatherName" type="text" class="form-control" />
+          <input v-model="info.fatherName" type="text" class="form-control" required />
 
           <label>تاریخ تولد</label>
-          <input v-model="info.birthdayDate" type="text" class="form-control" />
+          <input v-model="info.birthdayDate" type="text" class="form-control" required />
 
           <label>سمت</label>
-          <input v-model="info.role" type="text" class="form-control" />
+          <input v-model="info.role" type="text" class="form-control" required />
 
           <label>محل خدمت</label>
           <select class="form-control" v-model="info.employmentPlace">
@@ -23,10 +23,10 @@
           </select>
 
           <label>کد ملی</label>
-          <input v-model="info.idCard" type="number" class="form-control" />
+          <input v-model="info.idCard" type="number" class="form-control" required />
 
           <label>شماره شناسنامه</label>
-          <input v-model="info.idNumber" type="number" class="form-control" />
+          <input v-model="info.idNumber" type="number" class="form-control" required />
 
           <label>تحصیلات</label>
           <select class="form-control" v-model="info.education">
@@ -42,13 +42,37 @@
           </select>
 
           <label>آدرس</label>
-          <input v-model="info.address" type="text" class="form-control" />
+          <input
+            v-model="info.address"
+            type="text"
+            class="form-control"
+            style="margin-bottom:15px"
+            required
+          />
 
           <label>تاریخ شروع قرارداد</label>
-          <input v-model="info.startDate" type="text" class="form-control" />
-
-          <label>تاریخ پایان قرارداد</label>
-          <input v-model="info.endDate" type="text" class="form-control" />
+          <div class="form-row">
+            <div class="col">
+              <input
+                type="number"
+                class="form-control"
+                placeholder="Month"
+                v-model="info.startDate.month"
+                required
+              />
+            </div>
+            <div class="col">
+              <input
+                type="number"
+                class="form-control"
+                placeholder="Day"
+                v-model="info.startDate.day"
+                required
+              />
+            </div>
+          </div>
+          <label>مدت قرارداد</label>
+          <input v-model="info.periodDate" type="number" class="form-control" required />
 
           <label>حقوق</label>
           <select class="form-control" v-model="info.salary">
@@ -60,7 +84,7 @@
             <option>6</option>
           </select>
         </div>
-        <button type="button" @click="submitForm" class="btn btn-primary">ثبت</button>
+        <input id="submitBtn" type="submit" @click="submitForm" class="btn btn-primary" value="ثبت" />
       </div>
     </form>
   </div>
@@ -86,7 +110,9 @@ export default {
           worker: "2",
           housing: "2",
           children: "2",
-          sumOfSalary: "2"
+          sumOfSalary: "2",
+          salaryLetter:
+            "بیست و سه میلیون و سیصد و پنجاه و چهار هزار و دویست و هفتاد ریال"
         },
         {
           baseSalary: "a3",
@@ -94,7 +120,9 @@ export default {
           worker: "3",
           housing: "3",
           children: "3",
-          sumOfSalary: "3"
+          sumOfSalary: "3",
+          salaryLetter:
+            "بیست و سه میلیون و سیصد و پنجاه و چهار هزار و دویست و هفتاد ریال"
         }
       ],
       info: {
@@ -107,8 +135,11 @@ export default {
         idNumber: "",
         education: "",
         address: "",
-        startDate: "",
-        endDate: "",
+        startDate: {
+          month: "",
+          day: ""
+        },
+        periodDate: 1,
         salary: ""
       },
       status: false
@@ -120,6 +151,7 @@ export default {
 
   methods: {
     submitForm() {
+      this.info.periodDate--;
       for (let i = 0; i < this.salaryList.length; i++) {
         const selectedSalary = this.salaryList[i];
         if (selectedSalary.sumOfSalary == this.info.salary) {
@@ -133,3 +165,5 @@ export default {
   }
 };
 </script>
+
+
