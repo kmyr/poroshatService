d17ef6133b05c84c686ce7ac5f85a3baa8f73d02
kmyr <kmyr.rp@gmail.com>
2020-08-01 12:32:28 +0430

    fillout datastore files

---
+++ src/actions/getData.vue
@@ -0,0 +1,38 @@
+import db from "../datastore/firebaseInit";
+export default {
+  name: "dashboard",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    getData(document, arr) {
+      db.collection(document)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = {
+              
+            };
+            arr.push(data);
+          });
+        });
+    },
+    getCr(document, arr) {
+      db.collection(document)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = {
+              id: doc.id,
+              name: doc.data().name,
+              weight: doc.data().weight,
+              type: doc.data().type
+            };
+            arr.push(data);
+          });
+        });
+    }
+  }
+};
--- src/datastore/database-config.js
+++
@@ -1,8 +0,0 @@
-const db = firebase.initializeApp({
-    apiKey: '<your-api-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-url>',
-    projectId: '<your-cloud-firestore-project>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-sender-id>'
-});
