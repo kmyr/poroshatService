a5523f52f86aa3b0b59347d5a9eb060d4f33a016
Kamyar Poureatesam <kmyr.rp@gmail.com>
2020-08-03 23:52:28 +0430

    version 2.4.5 add update and delete action files

---
+++ src/actions/delete.vue
@@ -0,0 +1,17 @@
+<script>
+import db from "../components/firebaseInit";
+export default {
+  methods: {
+    async deleteData(doc, i) {
+      db.collection(doc)
+        .where("title", "==", i)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            doc.ref.delete();
+          });
+        });
+    }
+  }
+};
+</script>
---
+++ src/actions/update.vue
@@ -0,0 +1,19 @@
+<script>
+import db from "../components/firebaseInit";
+export default {
+  methods: {
+    updateData(obj, document) {
+      db.collection(document)
+        .where("name", "==", obj.name)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            doc.ref.update(obj).then(() => {
+              location.reload();
+            });
+          });
+        });
+    }
+  }
+};
+</script>
